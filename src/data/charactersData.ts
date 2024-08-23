37ca8f89a36bd0c666720791dfa72d294a114cb7
predador2000ori <navil-buchain@hotmail.com>
2024-08-23 16:05:32 -0600

    Added new characters to list  On branch main  Changes to be committed: 	modified:   src/data/charactersData.ts

--- src/data/charactersData.ts
+++ src/data/charactersData.ts
@@ -36,6 +36,62 @@ export const attackers = [
 			"Smoke grenade",
 			"Claymore"
 		]
+	},
+	{
+		name: "Twitch",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3YgCpOSM2R4SDgExstxm7P/ff84e6ac53bd9f690deee78870f9c23b/Y0R6_BADGE_Twitch_L.png",
+		weapons: {
+			primaryWeapon: [
+				"F2",
+				"417",
+				"SG-CQB"
+			],
+			secondaryWeapon: [
+				"P9",
+				"LFP586"
+			],
+		},
+		hability: ["Shock Drone"],
+		gadget: [
+			"Smoke grenade",
+			"Claymore"
+		]
+	},
+	{
+		name: "Buck",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/2oZZBEZzVLFpRY7Mp85MWq/f62287872dd61b4e0697a93d703b2beb/Y1S1_BADGE_Buck_L.png",
+		weapons: {
+			primaryWeapon: [
+				"C8-SFW",
+				"CAMRS"
+			],
+			secondaryWeapon: [
+				"MK1 9MM"
+			],
+		},
+		hability: ["Skeleton key"],
+		gadget: [
+			"Stunt grenade",
+			"Hard breach charge"
+		]
+	},
+	{
+		name: "Ying",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6ZVB1OJxNgs0pR0Fd7Kzt0/74721fe9fd743167508f98dc280a17a3/Y2S2_BADGE_Ying_L.png",
+		weapons: {
+			primaryWeapon: [
+				"T-95 LSW",
+				"SIX12"
+			],
+			secondaryWeapon: [
+				"SIX12"
+			],
+		},
+		hability: ["Candela"],
+		gadget: [
+			"Hard breach charge",
+			"Smoke grenade"
+		]
 	}
 ]
 
@@ -77,6 +133,63 @@ export const defenders = [
 		gadget: [
 			"Proximity alarm",
 			"Bulletproof camera"
+		]
+	},
+	{
+		name: "Azami",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/7KWTXHTk8rxLS5qtyX4Xu4/b51f8691b932fd2b83260ef3fbcd1154/r6s-operators-badge-azami.png",
+		weapons: {
+			primaryWeapon: [
+				"9X19SVN",
+				"ACS12"
+			],
+			secondaryWeapon: [
+				"D-50"
+			]
+		},
+		hability: ["kiba Barrier"],
+		gadget: [
+			"Impact Grenade",
+			"Barbed Wire"
+		]
+	},
+	{
+		name: "Ela",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/7zbkJJWMCOj04DNhYDz994/d2cba213d30d7b8cf2ba8cd568c78f20/Y2S3_BADGE_Ela_L.png",
+		weapons: {
+			primaryWeapon: [
+				"Scorpion EVO 3 A1",
+				"FO-12"
+			],
+			secondaryWeapon: [
+				"RG15"
+			]
+		},
+		hability: ["Grzmot mine"],
+		gadget: [
+			"Barbed Wire",
+			"Deployable shield",
+			"Impact Grenade"
+		]
+	},
+	{
+		name: "Pulse",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/ChAeJzdmwxuvhZTrV81rK/8c16116f4c719a3df864df6639e2f52e/Y0R6_BADGE_Pulse_L.png",
+		weapons: {
+			primaryWeapon: [
+				"UMP45",
+				"M1014"
+			],
+			secondaryWeapon: [
+				"5.7 USG",
+				"M45 MEUSOC"
+			]
+		},
+		hability: ["Grzmot mine"],
+		gadget: [
+			"Nitro Cell",
+			"Deployable shield",
+			"Observation blocker"
 		]
 	}
 ]
