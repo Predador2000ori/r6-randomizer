b83a5f08897616c0e15fb897435bf08f80cb3c01
predador2000ori <navil-buchain@hotmail.com>
2024-08-23 17:48:27 -0600

    Fixed multi operator randomizer  On branch main  Changes to be committed: Added functions to randomly map character loadout. 	modified:   src/components/CharacterSelector.tsx

--- src/components/CharacterSelector.tsx
+++ src/components/CharacterSelector.tsx
@@ -53,23 +53,54 @@ const CharacterSelector: React.FC = () => {
 };
 
 const CharacterSelectorSquad: React.FC = () => {
+	// selected character state
 	const [selectedCharacters, setSelectedCharacters] = useState<any>([]);
 
+	// Logic to random character selection
+	// Function to return random element from array
+	const getRandomElement = (array: any[]) => array[Math.floor(Math.random() * array.length)];
+	// Function to randomly sorts character array
 	const getUniqueRandomCharacters = (characters: any[], count: number) => {
-		const shuffled = [...characters].sort(() => 0.5 - Math.random());
-		return shuffled.slice(0, count);
+		const shuffled = ([...characters].sort(() => 0.5 - Math.random())).slice(0, count);
+		const setted = randomLoadout(shuffled);
+		return setted;
+	};
+	// Function to  set character loadout
+	const  randomLoadout = ( characters: any[]) => {
+		const characterLoaded = characters.map(character => ({
+			...character,
+			weapons: {
+				primaryWeapon: getRandomElement(character.weapons.primaryWeapon),
+				secondaryWeapon: getRandomElement(character.weapons.secondaryWeapon),
+			},
+			hability: getRandomElement(character.hability),
+			gadget: getRandomElement(character.gadget),
+		}));
+		return characterLoaded;
 	};
 
+	// Function to handle character selection
 	const handleSelectCharacters = (characterType: 'attacker' | 'defender') => {
+		// Sets the quantity of characters
+		let quantity: number = parseInt((document.getElementById("count") as HTMLInputElement).value);
+		quantity = quantity ? quantity : 1;		// case wher the user doesn't write anything
+		// Chose attackers or defenders
 		const characterList = characterType  === 'attacker' ? attackers : defenders;
-		const uniqueCharacters = getUniqueRandomCharacters(characterList, 5);
+		// Sorts the characters and trims the array to the quantity
+		const uniqueCharacters = getUniqueRandomCharacters(characterList, quantity);
+		// Sets the state
 		setSelectedCharacters(uniqueCharacters);
 	};
 
 	return (
 		<div>
-			<button onClick={() => handleSelectCharacters('attacker')}>Select 5 random attackers</button>
-			<button onClick={() => handleSelectCharacters('defender')}>Select 5 random defenders</button>
+			<p>
+				<label htmlFor="Quantity" >Quantity:</label>
+				<br />
+				<textarea name="Count" id="count" itemType="number"></textarea>
+			</p>
+			<button onClick={() => handleSelectCharacters('attacker')}>Random attackers</button>
+			<button onClick={() => handleSelectCharacters('defender')}>Random defenders</button>
 			<div>
 				{selectedCharacters.map((character: any, index: number) => (
 					<CharacterCard key={index} {...character} />
