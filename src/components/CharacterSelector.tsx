f002f2450adb499d9725174804bda2dacae4aa44
predador2000ori <navil-buchain@hotmail.com>
2024-08-23 13:52:08 -0600

    Characters are now displayed randomly with random loadout  On branch main  Changes to be committed: Changed CharacterCardProps to have multiple weapons and gatgets 	modified:   src/components/CharacterCard.tsx Removed unnecesary pictures 	deleted:    src/components/CharacterMedia/Attackers/Brava/logo.webp 	deleted:    src/components/CharacterMedia/Attackers/Iana/logo.webp 	deleted:    src/components/CharacterMedia/Defenders/Castle/logo.webp 	deleted:    src/components/CharacterMedia/Defenders/Echo/logo.webp Created methods to randomly select character, primary and secondary weapons, hability and gadgets 	modified:   src/components/CharacterSelector.tsx Added api example with two attackers and two defenders 	new file:   src/data/charactersData.ts

--- src/components/CharacterSelector.tsx
+++ src/components/CharacterSelector.tsx
@@ -1,34 +1,52 @@
 import React, { useState } from "react";
 import CharacterCard from "./CharacterCard";
-import EchoIconN from "./CharacterMedia/Defenders/Echo/logo.webp";
-import IanaLogo from "./CharacterMedia/Attackers/Iana/logo.webp";
+import { attackers, defenders } from "../data/charactersData";
 
 const CharacterSelector: React.FC = () => {
 	const [selectedCharacter, setSelectedCharacter] = useState({
-		name: 'unknown',
-		imageUrl: EchoIconN,
-		primaryWeapon: 'unknown',
-		secondaryWeapon: 'unknown',
-		hability: 'unknown',
-		gadget: 'unknown'
+		name: "unknown",
+		imageUrl: "https://via.placeholder.com/150",
+		weapons: {
+			primaryWeapon: ["unknown"],
+			secondaryWeapon: ["unknown"],
+		},
+		hability: ["unknown"],
+		gadget: ["unknown"]
 	});
 
-	const handleSelectCharacter = () => {
+	const handleSelectCharacter = (characterType: 'attacker' | 'defender') => {
 		// Logic to random character selection
-		const randomCharacter = {
-			name: 'Sample Character',
-			imageUrl: IanaLogo,
-			primaryWeapon: 'Sample Primary Weapon',
-			secondaryWeapon: 'Sample Secondary Weapon',
-			hability: 'Sample Hability',
-			gadget: 'Sample Gadget'
-		};
-		setSelectedCharacter(randomCharacter);
+		const characterList = characterType === 'attacker' ? attackers : defenders;
+		const randomIndex = Math.floor(Math.random() * characterList.length);
+		const randomCharacter = characterList[randomIndex];
+		const randomPrimaryWeapon = randomCharacter.weapons.primaryWeapon[
+			Math.floor(Math.random() * randomCharacter.weapons.primaryWeapon.length)
+		];
+		const randomSecondaryWeapon = randomCharacter.weapons.secondaryWeapon[
+			Math.floor(Math.random() * randomCharacter.weapons.secondaryWeapon.length)
+		];
+		const randomHability = randomCharacter.hability[
+			Math.floor(Math.random() * randomCharacter.hability.length)
+		];
+		const randomGadget = randomCharacter.gadget[
+			Math.floor(Math.random() * randomCharacter.gadget.length)
+		];
+		setSelectedCharacter({
+			name: randomCharacter.name,
+			imageUrl: randomCharacter.imageUrl,
+			weapons: {
+				primaryWeapon: [randomPrimaryWeapon],
+				secondaryWeapon: [randomSecondaryWeapon],
+			},
+			hability: [randomHability],
+			gadget: [randomGadget],
+		});
 	};
 
 	return (
 		<div>
-			<button onClick={handleSelectCharacter}>Select Random Character</button>
+			<button onClick={() => handleSelectCharacter('attacker')}>Select random attacker</button>
+			<button onClick={() => handleSelectCharacter('defender')}>Select random defender</button>
 			<CharacterCard {...selectedCharacter} />
 		</div>
 	);
---
+++ src/data/charactersData.ts
@@ -0,0 +1,82 @@
+export const attackers = [
+	{
+		name: "Iana",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6deSi1vse5iJTkErFksrGq/75bf1038e9698b6b2be75aa0493b092d/Y5S1_BADGE_Iana_L.png",
+		weapons: {
+			primaryWeapon: [
+				"G36C",
+				"ARX200"
+			],
+			secondaryWeapon: [
+				"GONNE-6",
+				"MK1 9MM"
+			]
+		},
+		hability: ["Gemini replicator"],
+		gadget: [
+			"Smoke grenade",
+			"Impact EMP grenade"
+		]
+	},
+	{
+		name: "Brava",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/1JTzualMKZVGfdROg4XKh8/7989454e62b7972bfd88f09097cc148f/r6s-operators-badge-brava.png",
+		weapons: {
+			primaryWeapon: [
+				"PARA-308",
+				"CAMRS"
+			],
+			secondaryWeapon: [
+				"USP40",
+				"Super shorty"
+			],
+		},
+		hability: ["Kludge drone"],
+		gadget: [
+			"Smoke grenade",
+			"Claymore"
+		]
+	}
+]
+
+export const defenders = [
+	{
+		name: "Echo",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3MRqAdUybJ7GR9gX4wPAI4/992938a70a4e53b91478358634e77ca7/Y1S4_BADGE_Echo_L.png",
+		weapons: {
+			primaryWeapon: [
+				"MP5SD",
+				"Supernova"
+			],
+			secondaryWeapon: [
+				"Bearing 9",
+				"P229"
+			],
+		},
+		hability: ["Yokai"],
+		gadget: [
+			"Impact grenade",
+			"Deployable shield"
+		]
+	},
+	{
+		name: "Castle",
+		imageUrl: "https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/kzznLctjOsXJZAg0F2HPB/dea28fa008515d6ddaa0d2659f4dc253/Y0R6_BADGE_Castle_L.png",
+		weapons: {
+			primaryWeapon: [
+				"UMP45",
+				"M1014"
+			],
+			secondaryWeapon: [
+				"5.7 USG",
+				"Super shorty",
+				"M45 MEUSOC"
+			],
+		},
+		hability: ["Armor panel"],
+		gadget: [
+			"Proximity alarm",
+			"Bulletproof camera"
+		]
+	}
+]
